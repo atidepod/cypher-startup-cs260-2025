0ccd0fbb1871a3da75360b5771534c0941407e2f
atidepod <cs260aws@gmail.com>
2025-10-04 10:49:02 -0600

    got the links to work

--- cypher-react/src/app.jsx
+++ cypher-react/src/app.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login.jsx';
+import { About } from './about/about.jsx';
+import { Message } from './message/message.jsx';
+
+
 export default function App() {
   return (
-    <div className="container-main">
+    <BrowserRouter>
       <header className="container-fluid">
         <menu>
           <nav className="navbar navbar-expand-lg navbar-dark">
@@ -15,9 +21,9 @@ export default function App() {
               </button>
               <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
-                  <li className="nav-item"><span className="nav-link">Welcome User 1!</span></li>
-                  <li className="nav-item"><a className="nav-link" href="message.html">Messages</a></li>
-                  <li className="nav-item"><a className="nav-link active" href="about.html">About</a></li>
+                  <li className="nav-item"><a className="nav-link" href="login">Login</a></li>
+                  <li className="nav-item"><a className="nav-link" href="message">Messages</a></li>
+                  <li className="nav-item"><a className="nav-link active" href="about">About</a></li>
                   <li className="nav-item"><a className="nav-link" href="https://github.com/atidepod/cypher-startup-cs260-2025">Github</a></li>
                 </ul>
               </div>
@@ -27,13 +33,19 @@ export default function App() {
 
 
       </header>
-      <main>
-        stuff goes here
-      </main>
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/about' element={<About />} />
+        <Route path='/message' element={<Message />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
       <footer className="footer">
         © 2025 Cypher
       </footer>
-    </div>
+    </BrowserRouter>
 
   );
+}
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: LOL Return to sender. Address unknown.</main>;
 }
