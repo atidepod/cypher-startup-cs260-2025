6e82ec4c2921bfdbf5da187ad17be00e116136c3
atidepod <cs260aws@gmail.com>
2025-11-08 19:22:43 -0700

    edited the nave bar to access the github link prior to log in

--- cypher-startup/src/login/login.jsx
+++ cypher-startup/src/login/login.jsx
@@ -84,7 +84,7 @@ export function Login({ setSessionId, setUsername }) {
           {loadingWord ? "Loading random word..." : `Random word of the moment: "${randomWord}"`}
         </div>
         <div>
-          <p>Please enter any username and password. The navigation bar including the github link will unlock afterwards</p>
+          <p>Please enter any username and password. The navigation bar will unlock afterwards</p>
         </div>
       </div>
     </div>
